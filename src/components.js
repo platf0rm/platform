1b5dc373b8846b102efbd55e456f122b18e6316b
TheoryOfNekomata <kiiroifuriku@hotmail.com>
2017-03-23 17:06:32 +0800

    Fix layout, implement ripple

--- src/components.js
+++ src/components.js
@@ -8,4 +8,18 @@
 require('./style.scss');
 const mdc = require('material-components-web');
 
-mdc.autoInit();
+setTimeout(initInteractiveLists, 250);
+
+function initInteractiveLists() {
+    let i,
+        li,
+        interactiveListItems = document.getElementsByTagName('main')[0].querySelectorAll('.mdc-list-item');
+    for (i = 0; li = interactiveListItems[i]; i++) {
+        mdc.ripple.MDCRipple.attachTo(li);
+        // Prevent link clicks from jumping demo to the top of the page
+        li.addEventListener('click', function(evt) {
+            evt.preventDefault();
+        });
+    }
+}
+
